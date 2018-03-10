262a6b9b0b898bfca5d4b23d88e154a59124103a
Michael Loesler <loesler@applied-geodesy.org>
2018-03-10 19:49:40 +0100

    Clipboard function for tables
    - add copy to clipboard via Ctrl+C
    - note: the true values will be copied to clipboard

--- JAG3D/src/org/applied_geodesy/jag3d/ui/table/UITableBuilder.java
+++ JAG3D/src/org/applied_geodesy/jag3d/ui/table/UITableBuilder.java
@@ -1,23 +1,23 @@
 /***********************************************************************
-* Copyright by Michael Loesler, https://software.applied-geodesy.org   *
-*                                                                      *
-* This program is free software; you can redistribute it and/or modify *
-* it under the terms of the GNU General Public License as published by *
-* the Free Software Foundation; either version 3 of the License, or    *
-* at your option any later version.                                    *
-*                                                                      *
-* This program is distributed in the hope that it will be useful,      *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-* GNU General Public License for more details.                         *
-*                                                                      *
-* You should have received a copy of the GNU General Public License    *
-* along with this program; if not, see <http://www.gnu.org/licenses/>  *
-* or write to the                                                      *
-* Free Software Foundation, Inc.,                                      *
-* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.            *
-*                                                                      *
-***********************************************************************/
+ * Copyright by Michael Loesler, https://software.applied-geodesy.org   *
+ *                                                                      *
+ * This program is free software; you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 3 of the License, or    *
+ * at your option any later version.                                    *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program; if not, see <http://www.gnu.org/licenses/>  *
+ * or write to the                                                      *
+ * Free Software Foundation, Inc.,                                      *
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.            *
+ *                                                                      *
+ ***********************************************************************/
 
 package org.applied_geodesy.jag3d.ui.table;
 
@@ -41,13 +41,36 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 
-public abstract class UITableBuilder<T>  {
+public abstract class UITableBuilder<T> {
+
+	class TableKeyEventHandler implements EventHandler<KeyEvent> {
+
+		KeyCodeCombination copyKeyCodeCompination = new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_ANY);
+		public void handle(final KeyEvent keyEvent) {
+
+			if (copyKeyCodeCompination.match(keyEvent)) {
+				if(keyEvent.getSource() instanceof TableView) {
+					copySelectionToClipboard( (TableView<?>) keyEvent.getSource());
+					keyEvent.consume();
+				}
+			}
+		}
+	}
+
+
 	class TableCellChangeListener<S> implements ChangeListener<S> {
 		private final int columnIndex;
 		private final T rowData; 
@@ -55,7 +78,7 @@ public abstract class UITableBuilder<T>  {
 			this.columnIndex = columnIndex;
 			this.rowData = rowData;
 		}
-		
+
 		@Override
 		public void changed(ObservableValue<? extends S> observable, S oldValue, S newValue) {
 			if (rowData != null) {
@@ -63,42 +86,43 @@ public abstract class UITableBuilder<T>  {
 			}
 		}
 	}
-	
+
 	class TableCellEvent<S> implements EventHandler<CellEditEvent<T, S>> {
 		private final int columnIndex;
 		TableCellEvent(int columnIndex) {
 			this.columnIndex = columnIndex;
 		}
-		
+
 		@Override
 		public void handle(CellEditEvent<T, S> event) {
 			if (event.getTableColumn().isEditable()) {
-//				event.getTablePosition().getRow()
+				//				event.getTablePosition().getRow()
 				//int column = event.getTablePosition().getColumn();
-        		//setValue(event.getRowValue(), column, event.getNewValue());
+				//setValue(event.getRowValue(), column, event.getNewValue());
 				setValue(event.getRowValue(), this.columnIndex, event.getTablePosition().getRow(), event.getOldValue(), event.getNewValue());
 			}
 		}
 	}
-	
+
 	static FormatterOptions options = FormatterOptions.getInstance();
 	static I18N i18n = I18N.getInstance();
 	TableView<T> table = this.createTable();
-	
+
 	UITableBuilder() {}
-	
+
 	TableView<T> createTable() {
 		this.table = new TableView<T>();
 		ObservableList<T> tableModel = FXCollections.observableArrayList();
 		this.table.setItems(tableModel);
 		this.table.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
 		this.table.setTableMenuButtonVisible(false);
-//		this.table.setPlaceholder(new Text(i18n.getString("UITableBuilder.emptytable", "No content in table.")));
+		//		this.table.setPlaceholder(new Text(i18n.getString("UITableBuilder.emptytable", "No content in table.")));
 		this.table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		this.table.setOnKeyPressed(new TableKeyEventHandler());
 		tableModel.add(this.getEmptyRow());		
 		return table;
 	}
-	
+
 	static <T> Callback<TableColumn<T,Integer>, TableCell<T,Integer>> getIntegerCallback() {
 		return new Callback<TableColumn<T, Integer>, TableCell<T, Integer>>() {
 			@Override
@@ -107,9 +131,9 @@ public abstract class UITableBuilder<T>  {
 				tableCell.setAlignment(Pos.CENTER_RIGHT);
 				return tableCell;
 			}
-	    };
+		};
 	}
-	
+
 	static <T> Callback<TableColumn<T,Double>, TableCell<T,Double>> getDoubleCallback(CellValueType cellValueType) {
 		return new Callback<TableColumn<T, Double>, TableCell<T, Double>>() {
 			@Override
@@ -118,9 +142,9 @@ public abstract class UITableBuilder<T>  {
 				tableCell.setAlignment(Pos.CENTER_RIGHT);
 				return tableCell;
 			}
-	    };
+		};
 	}
-	
+
 	static <T> Callback<TableColumn<T,Double>, TableCell<T,Double>> getDoubleCallback(CellValueType cellValueType, boolean displayUnit) {
 		return new Callback<TableColumn<T, Double>, TableCell<T, Double>>() {
 			@Override
@@ -129,31 +153,31 @@ public abstract class UITableBuilder<T>  {
 				tableCell.setAlignment(Pos.CENTER_RIGHT);
 				return tableCell;
 			}
-	    };
+		};
 	}
-	
+
 	static <T> Callback<TableColumn<T,String>, TableCell<T,String>> getStringCallback() {
-        return new Callback<TableColumn<T,String>, TableCell<T,String>>() {
-            @Override
-            public TableCell<T,String> call(TableColumn<T,String> cell) {
-            	TableCell<T,String> tableCell = new EditableCell<T, String>(new EditableStringCellConverter());
-            	tableCell.setAlignment(Pos.CENTER_LEFT);
-            	return tableCell;
-            }
-        };
-    }
-	
+		return new Callback<TableColumn<T,String>, TableCell<T,String>>() {
+			@Override
+			public TableCell<T,String> call(TableColumn<T,String> cell) {
+				TableCell<T,String> tableCell = new EditableCell<T, String>(new EditableStringCellConverter());
+				tableCell.setAlignment(Pos.CENTER_LEFT);
+				return tableCell;
+			}
+		};
+	}
+
 	static <T> Callback<TableColumn<T,Boolean>, TableCell<T,Boolean>> getBooleanCallback() {
 		return new Callback<TableColumn<T, Boolean>, TableCell<T, Boolean>>() {
 			@Override
 			public TableCell<T, Boolean> call(TableColumn<T, Boolean> column) {
 				TableCell<T, Boolean> tableCell = new CheckBoxTableCell<T, Boolean>();
 				tableCell.setAlignment(Pos.CENTER);
-	            return tableCell;
-	        }
-	    };
+				return tableCell;
+			}
+		};
 	}
-	
+
 	void addContextMenu(TableView<T> table, ContextMenu contextMenu) {
 		table.setRowFactory(new Callback<TableView<T>, TableRow<T>>() {  
 			@Override  
@@ -165,7 +189,7 @@ public abstract class UITableBuilder<T>  {
 			}  
 		}); 
 	}
-	
+
 	void addDynamicRowAdder(TableView<T> table) {
 		table.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
@@ -186,20 +210,20 @@ public abstract class UITableBuilder<T>  {
 			}
 		});
 	}
-		
+
 	<S>TableColumn<T, S> getColumn(ColumnTooltipHeader header, Function<T, ObservableValue<S>> property, Callback<TableColumn<T, S>, TableCell<T, S>> callback, ColumnType type, int columnIndex, boolean editable) {
 		final TableCellEvent<S> tableCellEvent = new TableCellEvent<S>(columnIndex);
 		TableColumn<T, S> column = new TableColumn<T, S>();
 		Label columnLabel = header.getLabel();
-	    columnLabel.getStyleClass().add("column-header-label");
-	    columnLabel.setMaxWidth(Double.MAX_VALUE);
-	    columnLabel.setTooltip(header.getTooltip());
+		columnLabel.getStyleClass().add("column-header-label");
+		columnLabel.setMaxWidth(Double.MAX_VALUE);
+		columnLabel.setTooltip(header.getTooltip());
 
-	    column.setUserData(type);
-	    column.setEditable(editable);
-	    column.setGraphic(columnLabel);
-	    column.setMinWidth(25);
-	    column.setPrefWidth(125);
+		column.setUserData(type);
+		column.setEditable(editable);
+		column.setGraphic(columnLabel);
+		column.setMinWidth(25);
+		column.setPrefWidth(125);
 		column.setCellValueFactory(new Callback<CellDataFeatures<T, S>, ObservableValue<S>>() {
 			@Override
 			public ObservableValue<S> call(CellDataFeatures<T, S> param) {
@@ -219,8 +243,36 @@ public abstract class UITableBuilder<T>  {
 	public TableView<T> getTable() {
 		return this.table;
 	}
-	
+
+	public static void copySelectionToClipboard(TableView<?> table) {
+		StringBuilder clipboardString = new StringBuilder();
+
+		for (TablePosition<?,?> position : table.getSelectionModel().getSelectedCells()) {
+			int rowIndex = position.getRow();
+
+			if (rowIndex < 0 || rowIndex >= table.getItems().size())
+				continue;
+
+			for (TableColumn<?, ?> column : table.getColumns()) {
+				if (!column.isVisible())
+					continue;
+
+				Object cell = column.getCellData(rowIndex);
+
+				if (cell == null)
+					cell = " ";
+
+				clipboardString.append(cell.toString()).append('\t');
+			} 
+			clipboardString.append("\r\n");
+		}
+
+		final ClipboardContent clipboardContent = new ClipboardContent();
+		clipboardContent.putString(clipboardString.toString());
+		Clipboard.getSystemClipboard().setContent(clipboardContent);
+	}
+
 	public static Object getValueAt(TableView<?> table, int columnIndex, int rowIndex) {
-	    return table.getColumns().get(columnIndex).getCellObservableValue(rowIndex).getValue();
+		return table.getColumns().get(columnIndex).getCellObservableValue(rowIndex).getValue();
 	}
 }
