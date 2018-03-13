60fee065888efd3f55fa6631d928eb810d177513
Michael Loesler <loesler@applied-geodesy.org>
2018-03-13 21:31:56 +0100

    update version for RC 3
    - increase version number

--- JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
+++ JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
@@ -27,7 +27,7 @@ public class Version {
 	private final static Map<VersionType, Integer> versions = Map.of(
 			VersionType.ADJUSTMENT_CORE,   20180207,
 			VersionType.DATABASE,          20180312,
-			VersionType.USER_INTERFACE,    20180311
+			VersionType.USER_INTERFACE,    20180313
 	);
 	
 	private Version() {}
