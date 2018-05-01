5a8762fac334d346c3d6e7053467489727eb48e8
Michael Loesler <loesler@applied-geodesy.org>
2018-05-01 21:15:13 +0200

    increase version of ui
    - set to 20180501

--- JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
+++ JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
@@ -27,7 +27,7 @@ public class Version {
 	private final static Map<VersionType, Integer> versions = Map.of(
 			VersionType.ADJUSTMENT_CORE,   20180410,
 			VersionType.DATABASE,          20180430,
-			VersionType.USER_INTERFACE,    20180421
+			VersionType.USER_INTERFACE,    20180501
 	);
 	
 	private Version() {}
