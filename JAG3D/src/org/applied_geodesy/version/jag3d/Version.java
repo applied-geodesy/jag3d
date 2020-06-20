5dcc4a35557a9c7f8ba3294ddb1728b15d62534b
Michael Loesler <loesler@applied-geodesy.org>
2020-06-20 10:18:40 +0200

    v20200620
    - increased version number v20200620

--- JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
+++ JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
@@ -29,7 +29,7 @@ public class Version {
 	private final static Map<VersionType, Integer> versions = Map.of(
 			VersionType.ADJUSTMENT_CORE,   20200327,
 			VersionType.DATABASE,          20200327,
-			VersionType.USER_INTERFACE,    20200618
+			VersionType.USER_INTERFACE,    20200620
 	);
 	
 	private Version() {}
