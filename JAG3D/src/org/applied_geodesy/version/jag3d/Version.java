13dc4ab5dcac7da60c267c1d23a514d565bf3804
Michael Loesler <loesler@applied-geodesy.org>
2021-02-16 10:08:35 +0100

    v20210216 (#185)
    - increase version number v20210216

--- JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
+++ JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
@@ -29,7 +29,7 @@ public class Version {
 	private final static Map<VersionType, Integer> versions = Map.of(
 			VersionType.ADJUSTMENT_CORE,   20210112,
 			VersionType.DATABASE,          20210215,
-			VersionType.USER_INTERFACE,    20210214
+			VersionType.USER_INTERFACE,    20210216
 	);
 	
 	private Version() {}
