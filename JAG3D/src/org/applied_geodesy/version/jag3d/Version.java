4175f805d85460c9d05d4782eb214de2368a0518
Michael Lösler <loesler@applied-geodesy.org>
2023-02-04 11:40:38 +0100

    v20230204 (#417)
    - Version number increased v20130204
    - Visibility of new table column (number of observations) restricted to a-posteriori view

--- JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
+++ JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
@@ -29,7 +29,7 @@ public class Version {
 	private final static Map<VersionType, Integer> versions = Map.of(
 			VersionType.ADJUSTMENT_CORE,   20220930,
 			VersionType.DATABASE,          20230131,
-			VersionType.USER_INTERFACE,    20230117
+			VersionType.USER_INTERFACE,    20230204
 	);
 	
 	private Version() {}
