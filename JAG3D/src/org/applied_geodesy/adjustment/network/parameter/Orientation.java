71ff8a1f8745e9656ce657c77d3bd7f994c97fbf
Michael Loesler <loesler@applied-geodesy.org>
2024-06-05 18:47:38 +0200

    Orientation
    - Change data type int vs. double

--- JAG3D/src/org/applied_geodesy/adjustment/network/parameter/Orientation.java
+++ JAG3D/src/org/applied_geodesy/adjustment/network/parameter/Orientation.java
@@ -86,7 +86,7 @@ public class Orientation extends AdditionalUnknownParameter {
 
 		// Ermittle eine a-priori Orieniterung mittels der Lage-koorigierten Richtungen
 		if (this.isEnable() && this.estimateApproximationValue)
-			this.setValue( MathExtension.MOD(this.getValue() + this.advancedOrientation(), 2*Math.PI) );
+			this.setValue( MathExtension.MOD(this.getValue() + this.advancedOrientation(), 2.0*Math.PI) );
 	}
 	
 	/**
