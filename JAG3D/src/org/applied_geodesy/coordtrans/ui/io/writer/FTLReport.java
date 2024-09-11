8270ffc5e4e7553ec5fc271b087e018d25a6a6da
Michael Lösler <loesler@applied-geodesy.org>
2024-09-11 19:49:57 +0200

    FreeMarker (#524)
    - FreeMarker v2.3.33 update

--- JAG3D/src/org/applied_geodesy/coordtrans/ui/io/writer/FTLReport.java
+++ JAG3D/src/org/applied_geodesy/coordtrans/ui/io/writer/FTLReport.java
@@ -76,7 +76,7 @@ import javafx.application.HostServices;
 import no.uib.cipr.matrix.Matrix;
 
 public class FTLReport {
-	private final static Version VERSION = Configuration.VERSION_2_3_32;
+	private final static Version VERSION = Configuration.VERSION_2_3_33;
 	private FormatterOptions options = FormatterOptions.getInstance();
 	private Template template = null;
 	private static HostServices hostServices;
