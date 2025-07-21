9200630c19175facabf16a52af69f495134b851b
Michael Lösler <loesler@applied-geodesy.org>
2025-07-21 20:55:53 +0200

    v20250721 (#574)
    - version number v20250721 corrected to avoid confusion

--- JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
+++ JAG3D/src/org/applied_geodesy/version/jag3d/Version.java
@@ -29,7 +29,7 @@ public class Version {
 	private final static Map<VersionType, Integer> versions = Map.of(
 			VersionType.ADJUSTMENT_CORE,   20250621,
 			VersionType.DATABASE,          20240417,
-			VersionType.USER_INTERFACE,    20250707
+			VersionType.USER_INTERFACE,    20250721
 	);
 	
 	private Version() {}
--- JAG3D/src/org/applied_geodesy/version/juniform/Version.java
+++ JAG3D/src/org/applied_geodesy/version/juniform/Version.java
@@ -28,7 +28,7 @@ import org.applied_geodesy.version.VersionType;
 public class Version {
 	private final static Map<VersionType, Integer> versions = Map.of(
 			VersionType.ADJUSTMENT_CORE,   20250717,
-			VersionType.USER_INTERFACE,    20250707
+			VersionType.USER_INTERFACE,    20250721
 	);
 	
 	private Version() {}
