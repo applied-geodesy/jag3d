5e5877de274411870ad1458771b172cce7d5ee52
Michael Lösler <loesler@applied-geodesy.org>
2026-05-24 14:06:19 +0200

    Open report (#619)
    - Desktop together with hostServices is used to open report in standard application (e.g. HTML --> browser, TXT --> editor etc.)

--- JAG3D/src/org/applied_geodesy/jag3d/ui/io/writer/report/FTLReport.java
+++ JAG3D/src/org/applied_geodesy/jag3d/ui/io/writer/report/FTLReport.java
@@ -21,6 +21,7 @@
 
 package org.applied_geodesy.jag3d.ui.io.writer.report;
 
+import java.awt.Desktop;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URL;
@@ -165,8 +166,19 @@ public class FTLReport {
 			this.template.process(this.data, writer);
 			writer.flush();
 
-			if (hostServices != null && openFile)
-				hostServices.showDocument(report.toAbsolutePath().normalize().toString());
+			if (openFile && Files.exists(report)) {
+				if (Desktop.isDesktopSupported()) {
+					try {
+						Desktop desktop = Desktop.getDesktop();
+						if (desktop.isSupported(Desktop.Action.OPEN)) {
+							desktop.open(report.toAbsolutePath().normalize().toFile());
+							openFile = false;
+						}
+					} catch (Exception e) {}
+				}
+				if (openFile && hostServices != null)
+					hostServices.showDocument(report.toAbsolutePath().normalize().toUri().toString());
+			}
 		}
 	}
 
